fe8beecfcb24a8fba8465c6c9da4a81d8d73874a
Samuel Diaz <90120898+samuelgiorno@users.noreply.github.com>
2025-04-28 01:15:05 +0200

    Update SecondsCounter.jsx

--- src/js/components/SecondsCounter.jsx
+++ src/js/components/SecondsCounter.jsx
@@ -1,4 +1,3 @@
-// src/component/SecondsCounter.jsx
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClock } from "@fortawesome/free-solid-svg-icons";
